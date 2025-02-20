0e78ae45fdd8eec6600aa395da028cfab8685359
davidgarciafp <david.garcia.fernandez@inslapineda.cat>
2025-02-20 13:13:51 +0100

    add Nivel1 and Nivel2 scenes to the game

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { config } from "./config.js";
 import { PreloadScene } from "./scenes/PreloadScene.js";
 import { GameScene } from "./scenes/GameScene.js";
+import { Nivel1 } from "./scenes/Nivel1/Nivel1.js";
+import { Nivel2 } from "./scenes/Nivel2/Nivel2.js";
 
-config.scene = [PreloadScene, GameScene];
+config.scene = [PreloadScene, GameScene, Nivel1, Nivel2];
 
 new Phaser.Game(config);
--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,134 +1,16 @@
-import { createAnimations } from "../animations/animations.js";
-import { createMario } from "../entities/Mario.js";
-import { createGoomba } from "../entities/Goomba.js";
+import { Nivel1 } from './Nivel1/Nivel1.js';
+import { Nivel2 } from './Nivel2/Nivel2.js';
+
+
 
 export class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'GameScene' });
     }
-
     create() {
-        this.add.image(100, 50, 'cloud1').setOrigin(0, 0).setScale(0.5);
-        this.add.image(300, 100, 'cloud1').setOrigin(0, 0).setScale(0.3);
-
-        this.floor = this.physics.add.staticGroup();
-        let suelo = this.floor.create(0, this.sys.game.config.height - 16, 'floorbricks').setOrigin(0, 0.5).setScale(3).refreshBody();
-         // Obtener el ancho real del suelo escalado
-        let floorWidth = suelo.displayWidth;
-        let floorHeight = suelo.displayHeight;
-        suelo = this.floor.create(suelo.x + floorWidth + 100, this.sys.game.config.height - 16, 'floorbricks')
-            .setOrigin(0, 0.5)
-            .setScale(3)
-            .refreshBody();
-
-
-        suelo = this.floor.create(suelo.x + floorWidth, this.sys.game.config.height - 16, 'floorbricks')
-                .setOrigin(0, 0.5)
-                .setScale(3)
-                .refreshBody();
-
-        const blockHeight = 16; 
-        this.blocks = this.physics.add.staticGroup();
-        this.block = this.blocks.create(suelo.x + floorWidth / 2, this.sys.game.config.height - floorHeight - blockHeight, 'block')
-            .setOrigin(0, 0)
-            .setScale(3)
-            .refreshBody();
-
-        
-        for (let i = 0; i < 5; i++) {
-            suelo = this.floor.create(suelo.x + floorWidth, this.sys.game.config.height - 16, 'floorbricks')
-                .setOrigin(0, 0.5)
-                .setScale(3)
-                .refreshBody();
-        }
-
-         // Crear Mario usando la función externa
-        this.mario = createMario(this);
-
-        this.goomba = createGoomba(this, 1500, this.sys.game.config.height - 100);
-
-
-
-
-        // Agregar colisión entre Mario y el suelo
-        this.physics.add.collider(this.mario, this.floor)
-        this.physics.add.collider(this.mario, this.blocks)
-
-
-        this.physics.add.collider(this.goomba, this.mario, () => {
-            if (!this.mario.isDead) {
-                this.mario.isDead = true;
-                this.mario.anims.play('mario-dead');
-                this.sound.stopAll();
-                this.sound.add('dead', { volume: 0.5 }).play();
-
-
-                setTimeout(() => {
-                    this.mario.setVelocityY(-350);
-                }, 100);
-
-                setTimeout(() => {
-                    this.scene.restart();
-                }, 2000);
-            }
-        });
-
-        // Agregar colisión con el suelo
-        this.physics.add.collider(this.goomba, this.floor);
-        
-        this.physics.add.collider(this.goomba, this.blocks)
-
-        createAnimations(this);
-        this.keys = this.input.keyboard.createCursorKeys();
-        this.sound.add('theme', { volume: 0.5, loop: true }).play();
+        this.scene.start('Nivel1');
+        this.scene.start('Nivel2');
 
     }
 
-    update() {
-
-    
-        if (this.mario.isDead) return;
-
-        if (this.keys.left.isDown) {
-            this.mario.anims.play('mario-walk', true);
-            this.mario.x -= 5;
-            this.mario.flipX = true;
-        } else if (this.keys.right.isDown) {
-            this.mario.anims.play('mario-walk', true);
-            this.mario.x += 5;
-            this.mario.flipX = false;
-        } else {
-            this.mario.anims.play('mario-idle', true);
-        }
-
-        if (this.keys.up.isDown && this.mario.body.touching.down) {
-            this.mario.setVelocityY(-500);
-            this.sound.add('jump', { volume: 0.1 }).play();
-            
-        }
-        if (!this.mario.body.touching.down) {
-            this.mario.anims.play('mario-jump', true);
-        } 
-
-        if (this.mario.y >= this.sys.game.config.height) {
-            this.mario.isDead = true;
-            this.mario.anims.play('mario-dead');
-            this.mario.setCollideWorldBounds(false);
-            this.sound.stopAll();
-            this.sound.add('dead', { volume: 0.5 }).play();
-
-            setTimeout(() => {
-                this.mario.setVelocityY(-350);
-            }, 100);
-
-            setTimeout(() => {
-                this.scene.restart();
-            }, 2000);
-        }
-
-        this.goomba.anims.play('goomba-walk', true);
-
-
-
-    }
 }
